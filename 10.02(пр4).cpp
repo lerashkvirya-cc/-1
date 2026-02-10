86f213760abe441c40420333bdece26b8288643a
lerashkvirya-cc <lera.shkvirya@bk.ru>
2026-02-10 12:09:34 +0300

    пр4

---
+++ 10.02(пр4).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	cout << "¬ведите число ";
+	int number;
+	cin >> number;
+	int a = 1;
+	while (a < 9) {
+		cout << "решите пример: " << number << "x" << a << endl;
+		int answ;
+		cin >> answ;
+		if (answ == number * a) {
+			cout << "ответ: " << answ << endl;
+			cout << "пример решен правильно!" << endl;
+			a = a + 1;
+		} else {
+			cout << "ошибка, решение неверно.";
+			return 0;
+		}
+	}
+	int _; cin >> _;
+}
